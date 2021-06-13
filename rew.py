ac2bb9ac7d138e2631706a973a0d386535fcddb3
rohithhr55 <85796647+rohithhr55@users.noreply.github.com>
2021-06-13 17:19:37 +0530

    Update rew.py

--- rew.py
+++ rew.py
@@ -3,5 +3,5 @@ from selenium.webdriver.support.select import Select
 driver = webdriver.Chrome()
 driver.get("https://www.pib.gov.in/indexd.aspx")
 sel=Select(driver.find_element_by_xpath("/html/body/form/header/div[1]/div/div/div/div[2]/ul/li[2]/select"))
-sel.select_by_visible_text("PIB Bengaluru")
+sel.select_by_visible_text("PIB Bengaluru").click()
 #driver.find_element_by_xpath("/html/body/form/div[4]/div/nav/div/ul/li[5]/ul/li[1]/a").click()
